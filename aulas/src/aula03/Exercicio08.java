b140b076801057c18d39fd31536bfbced11b1ed5
Rita Severa <ritasevera@hotmail.com>
2023-10-24 10:31:18 +0100

    Execução de novos exercícios e correção de outros

---
+++ aulas/src/aula03/Exercicio08.java
@@ -0,0 +1,27 @@
+package aula03;
+
+import java.util.Scanner;
+
+public class Exercicio08 {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        double nota1, nota2, nota3, mediaP;
+
+        System.out.println("Insira a sua nota de português: ");
+        nota1 = input.nextInt();
+        System.out.println("Insira a sua nota de inglês: ");
+        nota2 = input.nextInt();
+        System.out.println("Insira a sua nota de matemática: ");
+        nota3 = input.nextInt();
+
+        mediaP = (nota1 * 0.25 + nota2 * 0.35 + nota3 * 0.40);
+
+        //mediaP = (num1 * 0.2 + num2 * 0.3 + num3 * 0.5);
+        if (mediaP >= 9.5) {
+            System.out.println(mediaP + " Está aprovado! Continue com a sua evolução!");
+        } else {
+            System.out.println(mediaP + " Está reprovado! Não desista!");
+        }
+    }
+}
