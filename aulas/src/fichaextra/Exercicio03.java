179dd214d93db20c53cdf3e4283de440f7692d39
Rita Severa <ritasevera@hotmail.com>
2023-10-26 19:38:14 +0100

    Execução dos exercícios extra

---
+++ aulas/src/fichaextra/Exercicio03.java
@@ -0,0 +1,21 @@
+package fichaextra;
+
+import java.util.Scanner;
+
+public class Exercicio03 {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        double valor, desconto;
+        System.out.println("Insira o valor do produto: ");
+        valor = input.nextDouble();
+
+        if (valor > 100){
+            desconto = valor * 0.10;
+            valor = valor - desconto;
+            System.out.println("Terá um desconto de 10%." + valor);
+        } else {
+            System.out.println("Para ter desconto, teria de fazer uma compra superior a 100€");
+        }
+    }
+}
---
+++ aulas/src/fichaextra/Exercicio06.java
@@ -0,0 +1,20 @@
+package fichaextra;
+
+import java.util.Scanner;
+
+public class Exercicio06 {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+//Crie um programa que solicite um ano e verifique se ele é bissexto (Tem 366 dias e acontece de 4 e 4 anos).
+        int ano;
+
+        System.out.println("Indique um ano que tenha sido significativo para si: ");
+        ano = input.nextInt();
+
+        if (ano % 4 == 0){
+            System.out.println("O ano é bissexto");
+        } else {
+            System.out.println("Não escolheu um ano bissexto");
+        }
+    }
+}
