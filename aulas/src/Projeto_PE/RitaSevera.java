929e8f48aeb0934eb83cbe758e3158d5906e4431
Rita Severa <ritasevera@hotmail.com>
2023-11-16 11:16:47 +0000

    Continuação do projeto

--- aulas/src/Projeto_PE/RitaSevera.java
+++ aulas/src/Projeto_PE/RitaSevera.java
@@ -2,59 +2,26 @@ package Projeto_PE;
 
 import java.util.Scanner;
 
-import static FichaPratica06_Funcoes.Exercicio03.triangulo;
-
 public class RitaSevera {
 
     /**
-     * Método que avalia se um número é triangular
+     * Método que avalia números triangulares múltiplos de 5
      * @param num número a ser avaliado
-     * @return true se for triangular; false se não for
+     * @return os números de 0 até 121 que sejam triangulares e múltiplos de 5
      */
-    public static int triangulo (int num) {
+    public static void lugaresVazios (int num) { //Delcaro função e passo num como parâmetro
 
         int soma = 0, contador = 1;
+        num = 121;
 
-        while (soma < num) {
-            soma = soma + contador;
-            contador ++ ;
-            if (soma == num){
-                return true;
-            }
-        }
-        return false;
-    }
-
-    public static void menuCliente ( ) {
-        Scanner input = new Scanner(System.in);
-
-        int opcao, telemovel, lugar = 0, estacionamento;
-        String nome, email;
-
-        do {
-            System.out.println("Selecione o que pretende fazer: ");
-            System.out.println("1 - Registar-se como nosso cliente");
-            System.out.println("2 - Procurar lugar de estacionamente na nossa loja física do Porto");
-            System.out.println("3 - Imprimir todos os títulos de jogos existentes na loja");
-            System.out.println("4 - Segundo uma editora à sua escolha, apresentaremos todas as categorias e respetivos jogos");
-            opcao = input.nextInt();
-
-            switch (opcao) {
-                case 1:
-                    System.out.println("\nIremos proceder ao seu registo como nosso cliente");
-                    System.out.print("Insira o seu nome: ");
-                    nome = input.next();
-                    System.out.print("Insira o seu contacto telefónico: ");
-                    telemovel = input.nextInt();
-                    System.out.println("Insira o seu email: ");
-                    email = input.next();
-                    System.out.println("Obrigada pelo seu registo: " + nome + telemovel + email);
-                    break;
-                case 2:
+        while (soma < num) { //enquanto a soma for menor que 121 o ciclo continua
+            soma = soma + contador;// adiciona o valor atual da soma de contador à soma
+            contador++;
+            if (soma <= num && soma % 5 == 0) {
+                System.out.println(soma);
             }
         }
     }
-
 
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
@@ -89,5 +56,34 @@ public class RitaSevera {
                     System.out.println("Operação inválida.\nEscolha entre administrador e cliente\n");
             }
         } while (escolha != 1 && escolha !=2);
+
+
+        int opcao, telemovel;
+        String nome, email;
+
+        do {
+        System.out.println("Selecione o que pretende fazer: ");
+        System.out.println("1 - Registar-se como nosso cliente");
+        System.out.println("2 - Procurar lugar de estacionamente na nossa loja física do Porto");
+        System.out.println("3 - Imprimir todos os títulos de jogos existentes na loja");
+        System.out.println("4 - Segundo uma editora à sua escolha, apresentaremos todas as categorias e respetivos jogos");
+        opcao = input.nextInt();
+
+        switch (opcao) {
+            case 1:
+                System.out.println("\nIremos proceder ao seu registo como nosso cliente");
+                System.out.print("Insira o seu nome: ");
+                nome = input.next();
+                System.out.print("Insira o seu contacto telefónico: ");
+                telemovel = input.nextInt();
+                System.out.println("Insira o seu email: ");
+                email = input.next();
+                System.out.println("Obrigada pelo seu registo: " + nome + telemovel + email);
+                break;
+            case 2:
+                System.out.println("Os lugares disponíveis no nosso parque de estacionamento: ");
+                lugaresVazios(121);
+            }
+        } while (opcao!= 3);
     }
 }
